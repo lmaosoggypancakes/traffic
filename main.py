095b538e45a0f300b8adbfb49fd71358661266d0
lmaosoggypancakes <macerajosef@gmail.com>
2024-06-14 09:30:08 -0700

    feat: primitive space-aware velocities

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+"""
+traffic simulator with python. simulates cars travelling along a randomly-generated city.
+each car travels with a velocity that follows the graph of poisson's integral e**(-x^2)
+so it basically speeds up then slows down at a logistic rate.
+- red lines are bidirectional (a car can go from a to b and b to a arbitrarily) (a->b and b->a are possible)
+- purple lines are unidirectional (either a->b or b->a, but not both) as indicated by the arrow
+- "intersection" of diagonals is not yet supported and i'm not sure if i want/need to
+- TODO: handle collisions of cars/slowing down to avoid collisions
+"""
 import pygame
 from traffic import *
 ARROW_SIZE = 10
-NUM_CARS = 15
+NUM_CARS = 512
+SIDES = 6
 
-city = City(25)
+city = City(2 ** SIDES)
 city.randomize_connections()
 
 m = CityManager(city)
@@ -13,21 +23,35 @@ cars = []
 for i in range(NUM_CARS):
     c = Car(m, random.randrange(0, m.city.num_nodes))
     c.choose_random_road()
+    c.go_to(round(random.random()*10, 2))
     cars.append(c)
+
+cf = CarFactory(cars)
 
 pygame.init()
 pygame.font.init()
 
-font = pygame.font.SysFont("Noto Sans", 11)
+font_norm = pygame.font.SysFont("Noto Sans", 11)
+font_xl = pygame.font.SysFont("Noto Sans", 24)
 screen = pygame.display.set_mode(m.get_window_dimensions())
 clock = pygame.time.Clock()
 running = True
+cars_enabled = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            print(pygame.K_SPACE)
+            if event.key == pygame.K_SPACE:
+                cars_enabled = not cars_enabled
 
     screen.fill("black")
+    title = font_xl.render("traffic simulation", False, (255,255,255))
+    screen.blit(title, (screen.get_size()[0]/2-75, 30))
+
+    title = font_xl.render(f"cars={NUM_CARS}, roads={2 ** SIDES}", False, (255,255,255))
+    screen.blit(title, (screen.get_size()[0]/2-100, 70))
     # draw roads
     connections = m.get_connections()
     for (i, j) in connections:
@@ -50,21 +74,19 @@ while running:
                 left = (midpoint[0]-ARROW_SIZE*math.cos(angle-math.pi/4), midpoint[1]-ARROW_SIZE*math.sin(angle-math.pi/4))
             pygame.draw.polygon(screen, (255, 0, 255), [left, midpoint, right, left])
             pygame.draw.line(screen, (255, 0, 255), (x_i, y_i), (x_j, y_j))
-            # s = font.render(f"({i},{j})", False, (255, 255, 0))
-            s = font.render(str(angle), False, (255, 255, 0))
-            screen.blit(s, (midpoint[0], midpoint[1]-15))
     
     # draw intersections
     for (i, (x, y)) in enumerate(nodes):
         pygame.draw.circle(screen, (255,255,255), (x,y), 5)
-        s = font.render(str(i), False, (255, 255, 255))
+        s = font_norm.render(str(i), False, (255, 255, 255))
         screen.blit(s, (x-20, y-20))
-    
+
     for car in cars:
         pygame.draw.circle(screen, (0, 255, 0), car.pos, 5)
-        if len(car.get_frontier()) != 0:
-            car.go()
-    
+        # if len(car.get_frontier()) != 0 and cars_enabled:
+            # car.go()
+    if cars_enabled:
+        cf.make_cars_go()
     pygame.display.flip()
     clock.tick(60)
 
--- traffic.py
+++ traffic.py
@@ -1,5 +1,6 @@
 import random
 import math
+import uuid
 from typing import List
 # 60 fps, 5 pts/frame = 120 pts/s
 MAX_VEL = 5
@@ -9,6 +10,7 @@ class City:
         self.num_nodes = num_nodes
         self.s = int(math.sqrt(num_nodes))
         self.graph = []
+        self.id = uuid.uuid4()
         for i in range(num_nodes):
             self.graph.append([])
 
@@ -84,11 +86,37 @@ class Car:
     def __init__(self, m: CityManager, start=0):
         self.m = m
         self.last_point=start
+        self.vel = 0
         self.pos = m.coord_of(start)
         self.line_along = None
         if 0 > start or m.city.num_nodes <= start:
             raise Exception("invalid starting point")
     
+    def go_to(self, dist):
+        if self.line_along is None:
+            return
+        (start, end) = self.line_along
+        if start is None or end is None:
+            raise Exception("can't travel without a selected start/end")
+        (x_i, y_i) = self.m.coord_of(start)
+        (x_f, y_f) = self.m.coord_of(end)
+        dx = x_f - x_i
+        dy = y_f - y_i
+        total_dist = math.dist((x_i, y_i), (x_f, y_f))
+        if dx == 0:
+            if dy > 0:
+                angle = -math.pi/2
+            else:
+                angle = math.pi/2
+        else:
+            angle = math.atan2(dy, dx)
+        
+        x = dist * math.cos(angle) + x_i
+        y = dist * math.sin(angle) + y_i
+        proposed_dist = math.dist((x_i, y_i), (x,y)) 
+        if proposed_dist > total_dist:
+            raise Exception(f"total_distance is greater than your current path: {proposed_dist}/{total_dist}")
+        self.pos = [x,y]
     def get_frontier(self):
         return self.m.city.can_go_to(self.last_point)
 
@@ -99,8 +127,10 @@ class Car:
         else: 
             self.line_along = (self.last_point, random.choice(frontier))
     
-    def go(self):
+    def go(self, vel: float):
+        self.vel = vel
         if self.line_along == None:
+            return
             raise Exception("need to choose a road to follow")
         next_point = self.m.coord_of(self.line_along[1])
         dist = math.dist(self.pos, next_point)
@@ -118,16 +148,63 @@ class Car:
             angle = math.pi/2 if y_j-y_i > 0 else -math.pi/2
         else:
             angle = math.atan2((y_j-y_i), (x_j-x_i))
-        
-        total_dist = math.dist(self.m.coord_of(self.last_point), next_point)
-        travelled = dist / total_dist
-        vel = MAX_VEL * math.e ** (-2*MAX_VEL * ((travelled - 0.5) ** 2))
+         
         self.pos[0] += vel*math.cos(angle)
         self.pos[1] += vel*math.sin(angle)
 
+    def __repr__(self):
+        return f"{self.pos}: {self.line_along[0]} -> {self.line_along[1]}"
 class CarFactory:
     def __init__(self, cars: List[Car]):
+        self.cars = cars
         pass
 
+    def vel_for(self, car: Car):
+        """
+        gives the velocity a given car should go at, based on its position,
+        distance travelled along its path, and the relative proximity of nearby cars.
+        idea: cars should NEVER crash.
+        """
+        if len(car.get_frontier()) == 0:
+            return 0
+        next_point = car.m.coord_of(car.line_along[1])
+        dist = math.dist(car.pos, next_point)
+        total_dist = math.dist(car.m.coord_of(car.last_point), next_point)
+        travelled = dist / total_dist
+        vel_naive = MAX_VEL * math.e ** (-2*MAX_VEL * ((travelled - 0.5) ** 2))
+        neighbors = self.neighbors(car)
+        if len(neighbors) == 0:
+            return vel_naive
+        closest = min(neighbors, key=lambda k:k[0].vel)
+        if closest[0].vel < vel_naive:
+            return closest[0].vel
+        return vel_naive
+
+    def neighbors(self, car: Car):
+        """
+        fin ds all cars in the city that are 
+        - along the same path
+        - ahead by a small margin
+        to a given car, such that it must "slow" its speed
+        """
+        res = []
+        for c in self.cars:
+            if c == car:
+                continue
+            if c.line_along == car.line_along:
+                # travelling in the same road AND direction
+                dist = math.dist(c.pos, car.pos)
+                # are they close enough together, and is c "ahead" of car?
+                start = car.m.coord_of(car.last_point)
+                proximity = math.dist(start, c.pos) - math.dist(start, car.pos)
+                if 0 <= proximity <= 20:
+                    res.append([c, proximity])
+        return res
+    def make_cars_go(self):
+        """
+        moves all cars
+        """
+        for car in self.cars:
+            car.go(self.vel_for(car))
     # TODO: handle car crashes (cars waiting at different intersections, starting at different points along
     # a path, etc)
